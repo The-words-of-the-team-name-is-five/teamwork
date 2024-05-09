01efc83b43e419f0d4c8848d8ce811a69ef01983
3484734610 <162297171+3484734610@users.noreply.github.com>
2024-05-09 23:19:11 +0800

    Add files via upload

---
+++ teamwork_1.0/源码/online-mall-backend/src/main/java/com/shanzhu/em/controller/RoleController.java
@@ -0,0 +1,25 @@
+package com.team.em.controller;
+
+import com.team.em.common.R;
+import com.team.em.utils.TokenUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 角色 控制层
+ *
+ */
+@RestController
+public class RoleController {
+
+    /**
+     * 查询当前用户角色
+     *
+     * @return 角色名
+     */
+    @PostMapping("/role")
+    public R<String> getUserRole() {
+        return R.success(TokenUtils.getCurrentUser().getRole());
+    }
+
+}
