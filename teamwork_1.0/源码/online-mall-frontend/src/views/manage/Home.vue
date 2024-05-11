2942534f1a645437b406dba784d57e3f9d9a5a52
ChronoArk <161593249+ChronoArk@users.noreply.github.com>
2024-05-11 19:57:42 +0800

    Add files via upload

---
+++ teamwork_1.0/源码/online-mall-frontend/src/views/manage/Home.vue
@@ -0,0 +1,17 @@
+<!--
+ * 后台管理欢迎页
+ *
+-->
+<template>
+<div style="text-align: center;width: 100%;font-size: 32px;">
+  <h1>网上购物商城后台管理系统</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "Home.vue"
+}
+
+</script>
