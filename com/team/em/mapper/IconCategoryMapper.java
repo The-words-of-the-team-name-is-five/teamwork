6d8d779830e2995cf2d3f20a54af992643e54959
weilong181 <161993848+weilong181@users.noreply.github.com>
2024-05-14 00:00:17 +0800

    Add files via upload

---
+++ com/team/em/mapper/IconCategoryMapper.java
@@ -0,0 +1,14 @@
+package com.team.em.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.team.em.entity.IconCategory;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 图片 - 分类关联持久层（mapper）
+ *
+ */
+@Mapper
+public interface IconCategoryMapper extends BaseMapper<IconCategory> {
+
+}
